2439c2d9969b6ebcea7080fe6a7e94c172cd6c00
wesley.araujo <wesley.sickert@gmail.com>
2020-06-16 15:51:08 -0300

    remove unused file

--- setup/ConfigureSwift.rb
+++ setup/ConfigureSwift.rb
@@ -52,7 +52,7 @@ module Pod
 
       # There has to be a single file in the Classes dir
       # or a framework won't be created
-      `touch Pod/Classes/ReplaceMe.swift`
+      # `touch Pod/Classes/ReplaceMe.swift`
 
       # The Podspec should be 8.0 instead of 7.0
       text = File.read("NAME.podspec")
